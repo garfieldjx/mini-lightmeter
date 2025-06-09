010e74e9b52939c0be551482e1ac005479348f8b
garfieldjx <chowjingxiang@hotmail.com>
2025-06-09 09:41:15 +0800

    Updates

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <BH1750.h>
+#include <math.h>
 
-// put function declarations here:
-int myFunction(int, int);
+float isoCorrection(float ev, float iso);
+float timeValue(float aperture, float isoEV);
+BH1750 lm;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Wire.begin(9,8);
+
+  lm.begin(BH1750::CONTINUOUS_HIGH_RES_MODE, 0x23, &Wire);
+  Serial.println(F("BH1750 Test"));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  float lux = lm.readLightLevel();
+  float ev = log2(lux/2.5);
+  //log_2 (lux/2.5)
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  Serial.print("EV: ");
+  Serial.print(ev);
+  Serial.println(" ev");
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+float isoCorrection(float ev, float iso){
+  return (ev + log2((iso/100)));
 }
